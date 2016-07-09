b32662a1a4e5b2d342e1243a10e5403360175c1c
vileda <vileda@vileda.cc>
2016-07-09 21:18:00 +0200

    add example to parent

---
+++ src/main/kotlin/cc/vileda/kalfor/example/KalforExample.kt
@@ -0,0 +1,33 @@
+package cc.vileda.kalfor.example
+
+import cc.vileda.kalfor.handler.CombineHandler
+import cc.vileda.kalfor.handler.SchemaValidationHandler
+import io.vertx.rxjava.core.AbstractVerticle
+import io.vertx.rxjava.core.RxHelper
+import io.vertx.rxjava.core.Vertx
+import io.vertx.rxjava.ext.web.Router
+import io.vertx.rxjava.ext.web.handler.BodyHandler
+import io.vertx.rxjava.ext.web.handler.CorsHandler
+
+
+internal class KalforVerticle(private val listenPort: Int) : AbstractVerticle() {
+
+    @Throws(Exception::class)
+    override fun start() {
+        val httpServer = vertx.createHttpServer()
+
+        val router = Router.router(vertx)
+        router.route().handler(CorsHandler.create("*").allowedHeader("authorization"))
+        router.route().handler(BodyHandler.create())
+
+        router.route().handler(SchemaValidationHandler())
+        router.route().handler(CombineHandler(vertx))
+
+        httpServer.requestHandler({ router.accept(it) }).listen(listenPort)
+    }
+}
+
+fun main(args: Array<String>) {
+    val vertx = Vertx.vertx()
+    RxHelper.deployVerticle(vertx, KalforVerticle(8080))
+}
